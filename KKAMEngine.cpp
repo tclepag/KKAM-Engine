bf14faeed55201743502f1f99c54d8d0e0dd0731
tyler <lepagetyler@hotmail.com>
2025-03-23 01:06:07 -0500

    Add project files.

---
+++ KKAMEngine.cpp
@@ -0,0 +1,10 @@
+// KKAMEngine.cpp : Defines the entry point for the application.
+//
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// Your application code here
+	return 0;
+}
